ca1105e0bae013aec8e579f07e74407454e46e3e
avnitkr <65587151+avnitkr@users.noreply.github.com>
2021-06-03 15:59:22 +1000

    Update 1_beagle.sh

--- LD Blocks/1_beagle.sh
+++ LD Blocks/1_beagle.sh
@@ -18,7 +18,6 @@ REF=/90days/uqakaur3/Reference/SPD_CN1K_CtgRN.fasta
 MIN_MAF=0.10 #filter : will keep SNP above this allele frequency (over all individuals)
 PERCENT_IND=0.75 #filter : will keep SNP with at least one read for this percentage of individuals
 MAX_DEPTH_FACTOR=3 #filter : will keep SNP with less than X time the no of individuals total coverage
-MIN_DEPTH=1
 
 #loading ANGSD
 module load angsd
@@ -33,5 +32,5 @@ MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
 angsd -P 15 \
       -dosaf 1 -GL 2 -doGlf 2 -doMaf 1 -doMajorMinor 1 -doCounts 1 \
       -anc $REF -rf $REGIONS \
-      -remove_bads 1 -minMapQ 30 -minQ 20 -minInd $MIN_IND -setMaxDepth $MAX_DEPTH -minMaf $MIN_MAF -setMinDepthInd $MIN_DEPTH \
+      -remove_bads 1 -minMapQ 30 -minQ 20 -minInd $MIN_IND -setMaxDepth $MAX_DEPTH -minMaf $MIN_MAF \
       -b $BAMS -out ngsld_chrnine
